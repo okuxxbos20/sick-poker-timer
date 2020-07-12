ec31502afcfe8cef51a2e20424b3ce2b1f5008de
OKU FUJISYOHI <okuxxbos20@gmail.com>
2020-07-13 00:25:48 +0900

    add Login component

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,26 +1,28 @@
 <template>
   <div class="main container">
-    <Timer/>
-    <Controller/>
-    <Infomation/>
-    <Setting/>
+    <Login />
+    <Timer />
+    <Controller />
+    <Infomation />
+    <Setting />
   </div>
 </template>
 
 <script>
+import Login from './Login.vue';
 import Timer from './Timer.vue';
 import Controller from './Controller.vue';
 import Infomation from './Infomation.vue';
 import Setting from './Setting.vue';
 
 export default {
-  components: { Timer, Controller, Infomation, Setting }
+  components: { Login, Timer, Controller, Infomation, Setting }
 }
 </script>
 
 <style lang="scss" scoped>
 .main {
-  padding: 40px 0 0 0;
+  padding: 0;
   background: #111;
 }
 </style>
