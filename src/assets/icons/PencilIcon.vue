3642321cc4f39372a7005cde6ea799db1945cdd4
OKU FUJISYOHI <okuxxbos20@gmail.com>
2020-07-29 14:29:03 +0900

    new file

---
+++ src/assets/icons/PencilIcon.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="icon">
+    <svg viewBox="0 0 16 16" class="bi bi-pencil" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+      <path fill-rule="evenodd" d="M11.293 1.293a1 1 0 0 1 1.414 0l2 2a1 1 0 0 1 0 1.414l-9 9a1 1 0 0 1-.39.242l-3 1a1 1 0 0 1-1.266-1.265l1-3a1 1 0 0 1 .242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z"/>
+      <path fill-rule="evenodd" d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 0 0 .5.5H4v.5a.5.5 0 0 0 .5.5H5v.5a.5.5 0 0 0 .5.5H6v-1.5a.5.5 0 0 0-.5-.5H5v-.5a.5.5 0 0 0-.5-.5H3z"/>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PencilIcon'
+}
+</script>
+
+<style lang="scss" scoped>
+.icon {
+  width: 55px;
+  height: 55px;
+  border-radius: 50%;
+  margin: 0 20px;
+  transition: 200ms;
+  path { opacity: 0.8; }
+}
+</style>
---
+++ src/assets/icons/PlusIcon.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="icon">
+    <svg viewBox="0 0 16 16" class="bi bi-plus" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+      <path fill-rule="evenodd" d="M8 3.5a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5H4a.5.5 0 0 1 0-1h3.5V4a.5.5 0 0 1 .5-.5z"/>
+      <path fill-rule="evenodd" d="M7.5 8a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0V8z"/>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PlusIcon'
+}
+</script>
+
+<style lang="scss" scoped>
+.icon {
+  width: 55px;
+  height: 55px;
+  border-radius: 50%;
+  margin: 0 20px;
+  transition: 200ms;
+}
+</style>
